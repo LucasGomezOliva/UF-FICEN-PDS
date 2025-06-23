bfd50440cd9fcb301696675b57eb5266cdd9e7ec
Lucas <66488063+LucasGomezOliva@users.noreply.github.com>
2025-06-22 21:52:09 -0300

    Fix

--- TP4/Guia_4_Ejercicio_4.m
+++ TP4/Guia_4_Ejercicio_4.m
@@ -4,27 +4,50 @@ clc;        % Consola limpia
 clear;      % Workspace limpio
 close all;  % Cierra otras ventanas
 
-L = 512;
-fs = 1e3;       % Frecuencia de muestreo del filtro
-ts = 1/fs;      %   Periodo de Muestreo
-nTs = 0:ts:(L-1)*ts;        % Tiempo discreto
+L = 1024;
+fs = 1e3;               %   Frecuencia de muestreo del filtro
+ts = 1/fs;              %   Periodo de Muestreo
+nTs = 0:ts:(L-1)*ts;    %   Tiempo discreto
 
-% Ruido blanco Gauseano
+desvio = 0.5;
+xk = randn(length(nTs),1)*desvio;
 
-mu = 0;
-sigma = 1;
-xk = sigma * rand(L,1)';
-
-Numz = [1];
+Numz = [1 0 0];
 Denz = [1 -1.2 0.6 ];
 
 Hz = tf(Numz,Denz,-1)
 
-dk = filter(Numz,Denz,xk);
+dk = filter(1,Denz,xk);
 
-P = 0;
+P = 2;
 Q = 2;
 
-[ B, A, yk, ek ] = ARMA_Adaptativo( xk', dk' ,P , Q );
+[ B, A, yk, ek ] = ARMA_Adaptativo( xk, dk ,P , Q );
 
 Hz_adap = tf(B',A',-1)
+
+figure(1)
+subplot(4,1,1)
+plot(xk)
+grid on;
+title('Señal de entrada')
+xlabel('n[]')
+ylabel('x[n]')
+subplot(4,1,2)
+plot(dk)
+grid on;
+title('Señal deseada')
+xlabel('n[]')
+ylabel('d[n]')
+subplot(4,1,3)
+plot(yk)
+grid on;
+xlabel('n[]')
+ylabel('y[n]')
+title('Señal de salida')
+subplot(4,1,4)
+plot(ek)
+grid on;
+xlabel('n[]')
+ylabel('e[n]')
+title('Señal de error')
