c80a9fe046c62b3f400144604b0ab70aab0c9102
Lucas <66488063+LucasGomezOliva@users.noreply.github.com>
2025-06-25 00:16:59 -0300

    Ejercicio

---
+++ Parciales/Filtro_Media_Movil.m
@@ -0,0 +1,52 @@
+clear 
+close all
+clc
+
+L = 5;
+
+B = zeros(1,L+2);
+
+B(1) = 1;
+B(L+2) = -1;
+B = B * 1/(L+1);
+
+A = [1 -1];
+
+[H,w] = freqz(B,A,1024,"whole");
+
+tf(B,A,-1)
+
+F1 = figure(1);
+set(F1,'position',[80 130 900 550],'Menubar','figure',...
+        'NumberTitle','off','name',...
+        'Filtro de Media Movil con L = 5 : Respuesta en Modulo y Fase');
+
+subplot(2,1,1)
+plot(w/pi,abs(H));
+grid on;
+title('Respuesta en modulo')
+xlabel('\omega / \pi')
+ylabel('|H(e{jw})|')
+subplot(2,1,2)
+plot(w/pi,angle(H));
+grid on;
+title('Respuesta en fase')
+xlabel('\omega / \pi')
+ylabel('∠H(e^{j\omega})')
+
+F2 = figure(2);
+set(F2,'position',[80 130 900 550],'Menubar','figure',...
+        'NumberTitle','off','name',...
+        'Filtro de Media Movil con L = 5 : Diagrama de polos y ceros');
+
+zplane(B,A);
+grid on;
+
+
+F3 = figure(3);
+set(F3,'position',[80 130 900 550],'Menubar','figure',...
+        'NumberTitle','off','name',...
+        'Filtro de Media Movil con L = 5 : Respuesta al impulso');
+
+impz(B,A);
+grid on;
